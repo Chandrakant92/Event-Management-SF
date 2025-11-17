e4955500f51bee132a179e5f91626a90cca837da
Chandrakant J. Gawali <chandrakantgawali92@gmail.com>
2025-11-17 17:20:04 +0530

    Updated .js file  for Salesforce CI test

--- force-app/main/default/lwc/meeting_Form/meeting_Form.js
+++ force-app/main/default/lwc/meeting_Form/meeting_Form.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
 
-export default class Meeting_Form extends LightningElement {}
+export default class Meeting_Form extends LightningElement {
+
+  renderedCallback() {
+    console.log('Rendered callback executed..!!');
+
+  }
+}
+
+function testBug() {
+  return true;
+  console.log("This line will never execute");
+}
